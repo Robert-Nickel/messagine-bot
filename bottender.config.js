4fc4954b96c3cdb261e79aad070b11be71c3df0f
Harun Sokullu <harunsokullu@gmail.com>
2021-03-01 00:07:10 +0300

    test access token secret

--- bottender.config.js
+++ bottender.config.js
@@ -5,7 +5,7 @@ module.exports = {
       sync: true,
       path: '/webhooks/messenger',
       pageId: process.env.MESSENGER_PAGE_ID,
-      accessToken: process.env.MESSENGER_ACCESS_TOKEN,
+      accessToken: '${{ secrets.MESSENGER_ACCESS_TOKEN }}',
       appId: process.env.MESSENGER_APP_ID,
       appSecret: process.env.MESSENGER_APP_SECRET,
       verifyToken: process.env.MESSENGER_VERIFY_TOKEN,
