773ed8499098ba0104eb0f4d8853b75bcac845df
Harun Sokullu <harunsokullu@gmail.com>
2021-03-04 15:37:13 +0300

    revert config

--- bottender.config.js
+++ bottender.config.js
@@ -1,25 +1,14 @@
 module.exports = {
-  // session: {
-  //   driver: 'mongo',
-  //   stores: {
-  //     mongo: {
-  //       url: process.env.MONGO_URL,
-  //       collectionName: 'sessions',
-  //     },
-  //   },
-  // },
-  // initialState: {},
   channels: {
     messenger: {
       enabled: true,
-      // sync: true,
+      sync: true,
       path: '/webhooks/messenger',
       pageId: process.env.MESSENGER_PAGE_ID,
       accessToken: process.env.MESSENGER_ACCESS_TOKEN,
       appId: process.env.MESSENGER_APP_ID,
       appSecret: process.env.MESSENGER_APP_SECRET,
       verifyToken: process.env.MESSENGER_VERIFY_TOKEN,
-      // fields: ['messages', 'messaging_postbacks', 'messaging_optins', 'messaging_referrals', 'messaging_handovers', 'messaging_policy_enforcement'],
     },
     line: {
       enabled: false,
@@ -29,7 +18,6 @@ module.exports = {
     },
     telegram: {
       enabled: true,
-      // sync: true,
       path: '/webhooks/telegram',
       accessToken: process.env.TELEGRAM_ACCESS_TOKEN,
     },
