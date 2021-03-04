238ad44100a68907f98b4c484d26b9c9fd6efc9f
Harun Sokullu <harunsokullu@gmail.com>
2021-03-04 22:09:28 +0300

    move index to src

--- index.js
+++ index.js
@@ -1,7 +1,1 @@
-module.exports = async function App(context) {
-  if (context.event.isText) {
-    await context.sendText(context.event.text);
-  } else {
-    await context.sendText('Not text input');
-  }
-}
+module.exports = require('./src');
