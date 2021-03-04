9ea04764744c493d71b522e49f3f51b279ca337f
Harun Sokullu <harunsokullu@gmail.com>
2021-03-04 16:27:03 +0300

    move to run server

--- server.js
+++ server.js
@@ -1,49 +1,23 @@
-const serverless = require('serverless-http');
-// const bodyParser = require('body-parser');
-// const express = require('express');
-const { bottender, initializeServer } = require('bottender');
+import serverless from 'serverless-http';
+import { bottender, initializeServer } from 'bottender';
 
-const app = bottender({
-  dev: process.env.NODE_ENV !== 'production',
-});
+await runServer();
 
-// const port = Number(process.env.PORT) || 5000;
+async function runServer() {
+  const app = bottender({
+    dev: process.env.NODE_ENV !== 'production',
+  });
+  const handle = app.getRequestHandler();
+  const server = initializeServer();
 
-const handle = app.getRequestHandler();
-
-// const server = express();
-// server.use(
-//   bodyParser.json({
-//     verify: (req, _, buf) => {
-//       req.rawBody = buf.toString();
-//     },
-//   })
-// );
-
-// server.all('*', (req, res) => {
-//   app.prepare().then(() => {
-//     return handle(req, res);
-//   });
-// });
-
-// server.listen(port, err => {
-//   if (err) {
-//     console.log(err);
-//     throw err;
-//   }
-//   console.log(`> Ready on http://localhost:${port}`);
-// });
-
-const server = initializeServer();
-
-if (server) {
-  server.all('*', (req, res) => {
-    app.prepare().then(() => {
+  if (server) {
+    server.all('*', (req, res) => {
+      await app.prepare();
       return handle(req, res);
     });
-  });
-  // server.listen(port);
-  module.exports.handler = serverless(server);
-} else {
-  throw new Error('Server initialize failed.')
+    // server.listen(port);
+    module.exports.handler = serverless(server);
+  } else {
+    throw new Error('Server initialize failed.')
+  }
 }
